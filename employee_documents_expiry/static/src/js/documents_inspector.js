2aadf4cc0086663f50fb8eae056c28394dd3f9ca
Felipe Villamizar <felipeodoo@gmail.com>
2019-11-20 23:12:05 -0400

    Listo herencia atributos de documents
    [ADD] se agregaron tres campos a la vista camba cuando se selecciona un archivo

--- employee_documents_expiry/static/src/js/documents_inspector.js
+++ employee_documents_expiry/static/src/js/documents_inspector.js
@@ -3,14 +3,19 @@ odoo.define('employee_documents_expiry.DocumentsInspector', function (require) {
 
 var DocumentsInspector = require('documents.DocumentsInspector');
 DocumentsInspector.include({
+    /**
+     * @override
+     */
     _renderFields: function () {
         this._super.apply(this, arguments);
         var options = {mode: 'edit'};
         if (this.records.length === 1) {
             this._renderField('employee_id', options);
-            console.log(this.records)
+            this._renderField('description', options);
+            this._renderField('expiry_date', options);
         }
         
     },
     })
+return DocumentsInspector
 });
