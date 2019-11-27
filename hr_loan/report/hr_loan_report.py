72b2c530ae43f4969862a5f2c43a5c8414a5fd9b
Victor <vjdavila10.vd@gmail.com>
2019-11-27 17:27:55 -0400

    [ADD] Se crean los archivos para el reporte de prestamos [MOD] Se modifican el manifest el init y el modelo para para utilizar la logica del reporte

--- hr_loan/report/hr_loan_report.py
+++ hr_loan/report/hr_loan_report.py
@@ -8,15 +8,15 @@ import pytz
 from datetime import date, datetime, timedelta
 from odoo.exceptions import UserError, ValidationError
 
-class reportHrContract(models.TransientModel):
-    _name = 'report.payroll_mexico.contract_type_template'
+class reportHrLoan(models.TransientModel):
+    _name = 'report.hr_loan.loan_type_template'
         
     @api.multi
     def _get_report_values(self, docids, data=None):
         return {
             'doc_ids': docids,
-            'doc_model': 'hr.contract',
-            'docs': self.env['hr.contract'].search([('id','in',data['context']['active_ids'])]),
+            'doc_model': 'hr.loan',
+            'docs': self.env['hr.loan'].search([('id','in',data['context']['active_ids'])]),
             'data': data,
         }
         
