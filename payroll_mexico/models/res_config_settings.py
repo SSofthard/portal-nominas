8e2210b47548777d24e261acca752c78ad09a2ef
jonathan <jon.rey8995@gmail.com>
2019-12-04 14:40:15 -0600

    se quita default en tables para solventar error en base de datos

--- payroll_mexico/models/res_config_settings.py
+++ payroll_mexico/models/res_config_settings.py
@@ -9,7 +9,7 @@ class ResConfigSettings(models.TransientModel):
 
     tables_id = fields.Many2one(
         'tablas.cfdi', 'Tables',
-        related='company_id.tables_id', readonly=False)
+        readonly=False)
 
 class Company(models.Model):
     _inherit = 'res.company'
