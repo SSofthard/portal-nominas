0623912ceb431b3e2da98e20e8771f98c3fc7762
Jose Daniel Mancilla <josedmancillam@gmail.com>
2019-12-30 21:43:06 -0400

    [IMP] Cedula de autodeterminacion bimestral IMSS

--- payroll_mexico/report/fee_imss_employee_report.py
+++ payroll_mexico/report/fee_imss_employee_report.py
@@ -34,3 +34,30 @@ class reportHrFeeSettlement(models.TransientModel):
             'currency_precision': self.env.user.company_id.currency_id.decimal_places,
         }
         
+
+class reportHrFeeSettlementBimothly(models.TransientModel):
+    _name = 'report.payroll_mexico.hr_fees_employees_bimonthly'
+
+    @api.multi
+    def _get_report_values(self, docids, data=None):
+        docs = self.env['hr.fees.settlement'].search([('id','in',docids)])
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print ('kmdlskmdlksmdlsmdlskdml')
+        print (docs)
+        print (docs)
+        print (docs)
+        print (docs)
+        return {
+            'doc_ids': docids,
+            'doc_model': 'hr.contract',
+            'docs': docs,
+            'data': data,
+            'currency_precision': self.env.user.company_id.currency_id.decimal_places,
+        }
+
