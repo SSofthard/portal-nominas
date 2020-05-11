2c7354c7c8a71f40ba40c2b6e9f74e4431999acb
jonathan <jon.rey8995@gmail.com>
2020-05-11 12:41:13 -0400

    modulo de solicitudes

--- helpdesk_mexico/__manifest__.py
+++ helpdesk_mexico/__manifest__.py
@@ -5,10 +5,13 @@
     "author": "Soluciones SoftHard",
     "category": "Helpdesk",
     "website" : "http://www.solucionesofthard.com",
-    "depends": ['helpdesk','payroll_mexico'],
+    "depends": ['helpdesk','payroll_mexico','website_helpdesk','website_helpdesk_form'],
     "data": [ 
+            'security/helpdesk_security.xml',
             'views/helpdesk_ticket.xml',
+            'views/helpdesk_portal_templates.xml',
             'data/helpdesk_data.xml',
+            'data/website_helpdesk.xml',
     ],
 }
 
--- helpdesk_mexico/models/helpdesk_ticket.py
+++ helpdesk_mexico/models/helpdesk_ticket.py
@@ -11,6 +11,7 @@ from odoo.tools import pycompat
 class HelpdeskTicket(models.Model):
     _inherit = 'helpdesk.ticket'
     
+    ticket_type_id = fields.Many2one('helpdesk.ticket.type', string="Category")
     group_id = fields.Many2one('hr.group', "Grupo", store=True, required=False)
     stage_id = fields.Many2one('helpdesk.stage', string='Stage', ondelete='restrict', track_visibility='onchange',
                                group_expand='_read_group_stage_ids', copy=False,
@@ -21,3 +22,31 @@ class HelpdeskStage(models.Model):
     _inherit = 'helpdesk.stage'
     
     ticket_type_ids = fields.Many2many('helpdesk.ticket.type', string='Ticket type')
+
+class HelpdeskTicketTypeInherit(models.Model):
+    _inherit = 'helpdesk.ticket.type'
+    _parent_name = "parent_id"
+    _parent_store = True
+    _rec_name = 'complete_name'
+    
+    complete_name = fields.Char(
+        'Complete Name', compute='_compute_complete_name',
+        store=True)
+    parent_id = fields.Many2one('helpdesk.ticket.type', string='Parent')
+    parent_path = fields.Char(index=True)
+    child_id = fields.One2many('helpdesk.ticket.type', 'parent_id', 'Child Categories')
+    
+    @api.depends('name', 'parent_id.complete_name')
+    def _compute_complete_name(self):
+        for category in self:
+            if category.parent_id:
+                category.complete_name = '%s / %s' % (category.parent_id.complete_name, category.name)
+            else:
+                category.complete_name = category.name
+    
+    @api.multi
+    def name_get(self):
+        result = []
+        for tickey_type in self:
+            result.append((tickey_type.id, "%s" % (tickey_type.complete_name)))
+        return result
